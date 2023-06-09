478ad3d92d69666a0c660e215cffcbca066ef24b
Rudchenko <93388069+Rudchenko@users.noreply.github.com>
2023-06-09 23:09:09 +0300

    MyImage.py

--- MyImage.py
+++ MyImage.py
@@ -4,12 +4,6 @@ class MyImage:
     def __init__(self, path): 
         self.__path = path
         self.image = plt.imread(self.__path)
-        
-    def image_to_array(image): #поки ніде не використовується
-        return asarray(image)
-    
-    def array_to_image(array):  #поки ніде не використовується
-        return Image.fromarray(array)
     
     def get_rgb(self, x, y): 
         return self.image[y][x]
@@ -17,7 +11,7 @@ class MyImage:
     def set_rgb(self, x, y, r, g, b): 
         self.image[y][x] = r, g, b
 
-    def get_size(self): # тут лучче назвать get_shatpe
+    def get_size(self): 
         return self.image.shape
         
     def get_height(self):  
@@ -29,7 +23,7 @@ class MyImage:
     def get_path(self):  
         return self.__path
     
-    def get_image(self): #поки ніде не використовується
+    def get_image(self): 
         return self.image
 
     def set_image(self, new_image):
